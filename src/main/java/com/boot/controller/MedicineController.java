5548e93395c71d5e093faf8a71c3c045c027987f
KarolGorski <goralk2870@gmail.com>
2018-01-21 00:54:55 +0100

    Some DB done on H2. Controllers almost done too

--- src/main/java/com/boot/controller/MedicineController.java
+++ src/main/java/com/boot/controller/MedicineController.java
@@ -1,9 +1,8 @@
 package com.boot.controller;
 
+import com.boot.controller.MedicineStub;
 import com.boot.model.Medicine;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,9 +11,36 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("api/v1/")
+@RequestMapping("api/v1/") //ANGULAR TEGO WYMAGA!!!!!!!!!!!!!!!!!!!
 public class MedicineController {
 
-   // @RequestMapping(value = "medicines", method = RequestMethod.GET)
-   // public List<Medicine> list();
+    @RequestMapping(value = "medicines", method = RequestMethod.GET)
+    public List<Medicine> list()
+    {
+        return MedicineStub.list();
+    }
+
+    @RequestMapping(value = "medicines", method = RequestMethod.POST)
+    public Medicine create(@RequestBody Medicine medicine)
+    {
+        return MedicineStub.create(medicine);
+    }
+
+    @RequestMapping(value = "medicines/{id}", method = RequestMethod.GET)
+    public Medicine get(@PathVariable Long id)
+    {
+        return MedicineStub.get(id);
+    }
+
+    @RequestMapping(value = "medicines/{id}", method = RequestMethod.PUT)
+    public Medicine update(@PathVariable Long id, @RequestBody Medicine medicine)
+    {
+        return MedicineStub.update(id,medicine);
+    }
+
+    @RequestMapping(value = "medicines", method = RequestMethod.DELETE)
+    public Medicine delete(@PathVariable Long id)
+    {
+        return MedicineStub.delete(id);
+    }
 }
